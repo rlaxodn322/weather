9a6e0ab5fcf374d4206d3f6d9e7b7a1f809a573f
taewoo <rlaxodn322@gmail.com>
2023-09-19 20:55:41 +0900

    간단 날씨 프로젝트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=ba66cc78a9d58ba290d9b8d210f8f189`;
+  //ba66cc78a9d58ba290d9b8d210f8f189
+  return <div className="App"></div>;
 }
 
 export default App;

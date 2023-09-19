2f4e496dc2d7531981d996281a728f61e479dd6d
taewoo <rlaxodn322@gmail.com>
2023-09-19 21:10:55 +0900

    axios, API_KEY 등록

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
 function App() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=ba66cc78a9d58ba290d9b8d210f8f189`;
-  //ba66cc78a9d58ba290d9b8d210f8f189
-  return <div className="App"></div>;
+  const [location, setLocation] = useState("");
+  const API_KEY = "ba66cc78a9d58ba290d9b8d210f8f189";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+  const searchWeather = async (e) => {
+    if (e.key === "Enter") {
+      try {
+        const data = await axios({
+          method: "get",
+          url,
+        });
+        console.log(data);
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+  return (
+    <AppWrap>
+      <div className="appContentWrap">
+        <input
+          placeholder="도시를 입력하세요."
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          type="text"
+          onKeyDown={searchWeather}
+        />
+      </div>
+    </AppWrap>
+  );
 }
 
 export default App;
+
+const AppWrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border: 1px red solid;
+  .appContentWrap {
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    border: 1px blue solid;
+    padding: 20px;
+  }
+`;
